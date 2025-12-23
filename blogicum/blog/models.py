888ffa384c8996b4e71f8b560556614c6662bc51
Anton Malyshev <132440944+lolipolovkovith@users.noreply.github.com>
2025-12-23 20:22:40 +0300

    refactor

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -10,7 +10,10 @@ class PublishedModel(models.Model):
         verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
 
     class Meta:
         abstract = True
